594ca30894366d033f89305825bf0fe257dc4307
bermute <dl4946@naver.com>
2025-06-24 12:57:33 +0900

    자동 배포 테스트 1

--- src/main/java/com/abell/blog/controller/UserViewController.java
+++ src/main/java/com/abell/blog/controller/UserViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
-    @GetMapping("/login")
+    @GetMapping({"/login","/"})
     public String login() {
         return "oauthLogin";
     }
